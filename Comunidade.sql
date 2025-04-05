1ca704563b9b4927f8221697842eab95ccbd36da
Iago-Pereira <iagopereira@outlook.com>
2025-04-05 00:55:57 -0300

    feat: inserção de alunos

--- Comunidade.sql
+++ Comunidade.sql
@@ -1,6 +1,14 @@
 CREATE DATABASE comunidade;
 USE comunidade;
 
+INSERT INTO aluno (nome, nascimento) VALUES ('Naruto Uzumaki', '1994-02-15');
+INSERT INTO aluno (nome, nascimento) VALUES ('Gon Freecs', '1998-05-17');
+INSERT INTO aluno (nome, nascimento) VALUES ('Grisha Yeager', '1988-04-02');
+INSERT INTO aluno (nome, nascimento) VALUES ('Lovepreet Bolt', '1996-11-21');
+INSERT INTO aluno (nome, nascimento) VALUES ('Dale Bamba', '2022-07-15');
+
+SELECT * from aluno;
+
 /*
 DROP TABLE IF EXISTS aluno;
 CREATE TABLE aluno(
